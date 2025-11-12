3815af728d25277f0ff1391f63433b68f63fcba9
xXXTylerDurdenXXx <143999505+xXXTylerDurdenXXx@users.noreply.github.com>
2025-11-12 13:20:12 +0300

    добавил Middleware

---
+++ GameStatisticsVolleball/TestMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace GameStatisticsVolleball
+{
+    public class TestMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<TestMiddleware> _logger;
+
+        public TestMiddleware(RequestDelegate next , ILogger<TestMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                
+                _logger.LogError(ex, $"Произошло необработанное исключение: {ex.Message}");
+
+                
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = new
+                {
+                    StatusCode = context.Response.StatusCode,
+                    Message = "Произошла внутренняя ошибка сервера. Пожалуйста, попробуйте позже.",
+                    Details = ex.Message 
+                };
+
+                var json = JsonSerializer.Serialize(response);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
