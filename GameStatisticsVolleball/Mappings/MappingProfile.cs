91e5a178897c8e39417ec4254577960d882d1429
xXXTylerDurdenXXx <143999505+xXXTylerDurdenXXx@users.noreply.github.com>
2025-11-12 12:38:12 +0300

    создал профиль мапирования

---
+++ GameStatisticsVolleball/Mappings/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿namespace GameStatisticsVolleball.Mappings
+{
+    using AutoMapper;
+    using GameStatisticsVolleball.Models;
+    using GameStatisticsVolleball.Models.DTO;
+    public class MappingProfile: Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<CreatePlayerDTO, Player>();
+            CreateMap<UpdatePlayerDTO, Player>();
+
+            CreateMap<CreateTeamDTO, Team>();
+            CreateMap<UpdateTeamDTO, Team>();
+
+            CreateMap<CreateTournamentDTO, Tournament>();
+
+            CreateMap<CreatePositionDTO, Position>();
+
+            CreateMap<CreateMatchDTO, Match>();
+
+            CreateMap<CreatePlayerMatchStatsDTO, PlayerMatchStats>();
+            
+        }
+    }
+}
