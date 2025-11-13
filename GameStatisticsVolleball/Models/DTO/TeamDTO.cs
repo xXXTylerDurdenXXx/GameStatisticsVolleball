ca148a9ae6503ddc20cf87abb1e3a95a20748af5
xXXTylerDurdenXXx <143999505+xXXTylerDurdenXXx@users.noreply.github.com>
2025-11-13 14:51:50 +0300

    Создал TeamDTO для чтения

---
+++ GameStatisticsVolleball/Models/DTO/TeamDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace GameStatisticsVolleball.Models.DTO
+{
+    public class TeamDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string CoachName { get; set; } = null!;
+        public string City { get; set; } = null!;
+        
+    }
+}
