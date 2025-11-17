47f344113f91d393daf676296af45f20cff7abdf
xXXTylerDurdenXXx <143999505+xXXTylerDurdenXXx@users.noreply.github.com>
2025-11-17 08:37:34 +0300

    Сделал миграции с ролью и пользователем в базу данных

---
+++ GameStatisticsVolleball/Migrations/20251117053636_AddUserAndRole.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameStatisticsVolleball.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserAndRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
