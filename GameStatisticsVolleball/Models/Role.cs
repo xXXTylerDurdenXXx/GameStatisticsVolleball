8d23ef5ef97aa5dbc262b0301b2a5a32a49edef8
xXXTylerDurdenXXx <143999505+xXXTylerDurdenXXx@users.noreply.github.com>
2025-11-17 08:35:38 +0300

    сделал модели роль и пользователь

---
+++ GameStatisticsVolleball/Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStatisticsVolleball.Models
+{
+    public class Role
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; } = null!;
+        [StringLength(200)]
+
+        public List<User> Users { get; set; } = new List<User>();
+    }
+}
---
+++ GameStatisticsVolleball/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace GameStatisticsVolleball.Models
+{
+    [Index(nameof(Email), IsUnique = true)]
+    [Index(nameof(Login), IsUnique = true)]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Login { get; set; } = String.Empty;
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [MinLength(8)]
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdateAt { get; set; }
+        public bool IsActive { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; } = null!;
+    }
+}
+
+
