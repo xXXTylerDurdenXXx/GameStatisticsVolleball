d5c9eab6d1bf04fdca5b42002c343e8663457c33
xXXTylerDurdenXXx <143999505+xXXTylerDurdenXXx@users.noreply.github.com>
2025-11-19 13:57:00 +0300

    создал DTO для регистрации

---
+++ GameStatisticsVolleball/Models/DTO/RegisterRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStatisticsVolleball.Models.DTO
+{
+    public class RegisterRequest
+    {
+        [Required(ErrorMessage = "Введите Email")]
+        [EmailAddress(ErrorMessage = "Некорректный формат Email")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Введите имя пользователя")]
+        [MinLength(3, ErrorMessage = "Имя пользователя должно быть минимум 3 символа")]
+        public string UserName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Введите пароль")]
+        [MinLength(6, ErrorMessage = "Пароль должен быть минимум 6 символов")]
+        public string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = "Повторите пароль")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
